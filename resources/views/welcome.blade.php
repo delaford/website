cda16ba6e13783086e70bab0114689b5419fcdaa
Dan Jasnowski <djasnowski@gmail.com>
2019-06-19 00:58:57 -0500

    Align items to center.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,19 @@
 @section('content')
     <div class="empty">
         <div class="columns">
-            <div class="column col-7">
+            <div class="column col-7 text-left">
+                <h1>Hiscores</h1>
+                <ul class="tab tab-block">
+                    <li class="tab-item active">
+                        <a href="#">Mining</a>
+                    </li>
+                    <li class="tab-item">
+                        <a href="#" title="Coming soon!">Smithing</a>
+                    </li>
+                    <li class="tab-item">
+                        <a href="#" title="Also coming soon.">Fishing</a>
+                    </li>
+                </ul>
                 <table class="table table-striped">
                     <thead class="text-bold">
                         <tr>
@@ -25,8 +37,8 @@
                     </tbody>
                 </table>
             </div>
-            <div class="column col-5 flex content-center">
-                <div class="column col-12">
+            <div class="column col-5" style="display: flex; align-items: center">
+                <div class="col-12">
                     <p class="empty-subtitle">Register to play in seconds!</p>
                     <div class="empty-action">
                         <a href="{{ route('register') }}" class="btn btn-primary">Register</a> or <a href="https://play.delaford.com" class="btn btn-primary">Play Now</a>
